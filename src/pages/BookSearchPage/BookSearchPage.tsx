5ef1123752bf1f1d49e21bb491ae642cc17d801f
Lars Abelsson <georg483@msn.com>
2024-05-01 21:12:47 +0200

    BookSearchPage

---
+++ src/pages/BookSearchPage/BookSearchPage.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+import BookList from "../../components/BookList/BookList"
+import { useSelector } from "react-redux"
+import { RootState } from "../../state/store"
+
+const BookSearchPage = () => {
+  const [offset, setOffset] = useState(0)
+
+  const searchWord = useSelector(
+    (state: RootState) => state.searchField.searchWord
+  )
+
+  const Url = `https://openlibrary.org/search.json?title=${searchWord}&limit=10&offset=${offset}&type=work`
+
+  const handleNext = (resultNumber: number) => {
+    if (resultNumber > offset + 10) {
+      setOffset(offset + 10)
+    }
+  }
+  const handlePrev = () => {
+    if (offset > 0) {
+      setOffset(offset - 10)
+    }
+  }
+
+  return (
+    <>
+      {searchWord.length > 0 && (
+        <section className="search-book">
+          <article>
+            <BookList
+              url={Url}
+              handleNext={handleNext}
+              handlePrev={handlePrev}
+            />
+          </article>
+        </section>
+      )}
+    </>
+  )
+}
+
+export default BookSearchPage
--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom"
 import ErrorPage from "./pages/ErrorPage/ErrorPage"
 import Home from "./pages/Home/Home"
 import Layout from "./pages/Layout/Layout"
+import BookSearchPage from "./pages/BookSearchPage/BookSearchPage"
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/searchbook",
+        element: <BookSearchPage />,
       },
     ],
   },
